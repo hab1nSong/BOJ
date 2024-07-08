7e700a6912ba09cf38a3b64fd98533837866587e
Habin Song <93114062+hab1nSong@users.noreply.github.com>
2024-07-09 01:23:11 +0900

    [Bronze V] Title: A+B - 8, Time: 168 ms, Memory: 16424 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st;
+        
+        int firstLine = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=firstLine; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            bw.write("Case #" + i + ": " + A + " + " + B + " = " + (A+B) + "\n");
+        }
+        
+        bw.close();
+    }
+}
